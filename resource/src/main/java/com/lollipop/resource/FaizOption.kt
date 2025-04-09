7d627607e4e7cda45d6f61cba74917ee41e419b3
lollipop <liujian594@hotmail.com>
2025-04-09 23:28:32 +0800

    feat: 增加Faiz的一些指令，好难整理呀

--- resource/src/main/java/com/lollipop/resource/FaizOption.kt
+++ resource/src/main/java/com/lollipop/resource/FaizOption.kt
@@ -30,114 +30,172 @@ enum class FaizOption(val icon: Int, val code: String = "", val command: Int = 0
 
     /**
      * 单发模式
+     * Phone Blaster Single Mode
+     * 最多12发
      */
-    FzizTemp04(R.drawable.ic_logo, "103"),
+    PhoneBlasterSingleMode(R.drawable.ic_logo, "103"),
 
     /**
      * 连发模式
+     * 三连发，最多4次
+     * Phone Blaster Burst Mode，Phone Blaster的三连发模式
      */
-    FzizTemp06(R.drawable.ic_logo, "106"),
+    PhoneBlasterBurstMode(R.drawable.ic_logo, "106"),
 
     /**
      * 充能
+     * Phone Blaster Charge，让各发射模式下的Phone Blaster再次充填能量。
      */
-    FzizTemp07(R.drawable.ic_logo, "279"),
+    PhoneBlasterCharge(R.drawable.ic_logo, "279"),
 
     /**
      * 召唤巨型载具
+     *  呼叫Jet Sliger，输入后会发出“Jet Sliger come closer”的语音，
+     *  Jet Sliger会以自动驾驶的方式到达使用者身边。
      */
-    FzizTemp08(R.drawable.ic_logo, "3821"),
+    JetSligerComeCloser(R.drawable.ic_logo, "3821"),
+
+    /**
+     * 巨型载具
+     * 喷射滑行者 准备行动
+     */
+    JetSligerGetIntoAction(R.drawable.ic_logo, "3814"),
+
+    /**
+     * 巨型载具
+     * 喷射滑行者 起飞
+     */
+    JetSligerTakeOff(R.drawable.ic_logo, "3846"),
+
+
+    /**
+     * 加速手表
+     */
+    FaizAxelMode(R.drawable.ic_logo, "Axel"),
 
     /**
      * 真红大炮
      * Faiz Pointer(faiz光标) Put into motion
      * 功能存疑
      */
-    Temp5576(R.drawable.ic_logo, "5576"),
+    FaizPointer(R.drawable.ic_logo, "5576"),
+
     /**
      * 真红大炮
      * 深红电风扇
      * 爆裂faiz发动踢技必杀
      * Faiz Pointer(faiz光标)Exceed Charge
+     * 让Faiz直接使用Blaster Crimson Smash。
      */
-    Temp5532(R.drawable.ic_logo, "5532"),
+    FaizBlasterCrimsonSmash(R.drawable.ic_logo, "5532"),
+
     /**
      * 真红大炮
      * Faiz Shot(faiz拳套) Put into motion
      * 功能存疑
      */
-    Temp5276(R.drawable.ic_logo, "5276"),
+    FaizShot(R.drawable.ic_logo, "5276"),
+
     /**
      * 真红大炮
      * Faiz Shot(faiz拳套)Exceed Charge
+     * 让Faiz直接使用Blaster Grand Impact。
      */
-    Temp5232(R.drawable.ic_logo, "5232"),
+    FaizBlasterGrandImpact(R.drawable.ic_logo, "5232"),
+
     /**
      * 真红大炮
      * Faiz Edge (faiz利刃) Put into motion
      * 功能存疑
      */
-    Temp5476(R.drawable.ic_logo, "5476"),
+    FaizEdge(R.drawable.ic_logo, "5476"),
+
     /**
      * 真红大炮
      * Faiz Edge (faiz利刃)Exceed Charge
      */
-    Temp5432(R.drawable.ic_logo, "5432"),
+    FaizEdgeExceed(R.drawable.ic_logo, "5432"),
+
     /**
      * 真红大炮
      * 爆裂形态飞行
+     * Faiz Blaster Take Off
      */
-    Temp5246(R.drawable.ic_logo, "5246"),
+    FaizBlasterTakeOff(R.drawable.ic_logo, "5246"),
+
     /**
      * 真红大炮
      * 发射肩跑
      */
-    Temp5214(R.drawable.ic_logo, "5214"),
+    FaizBlasterDischarge(R.drawable.ic_logo, "5214"),
+
+    /**
+     * 真红大炮
+     * Blade Mode，Faiz Blaster会变成大剑型的Breaker Mode
+     */
+    FaizBlasterBladeMode(R.drawable.ic_logo, "143"),
+
+    /**
+     * 真红大炮
+     * Blaster Mode，Faiz Blaster会变成散弹枪型的Blaster Mode。
+     */
+    FaizBlasterBlasterMode(R.drawable.ic_logo, "103"),
 
     /**
      * 真红大炮、Faiz
      * 机动天马，准备行动
+     * Auto Vajin Get Into Action
      */
-    Temp5814(R.drawable.ic_logo, "5814"),
+    FaizAutoVajinGetIntoAction(R.drawable.ic_logo, "5814"),
+
     /**
      * 真红大炮、Faiz
      * 机动天马 靠近
+     * AutoVajin Come Closer
      */
-    Temp5821(R.drawable.ic_logo, "5821"),
+    FaizAutoVajinComeCloser(R.drawable.ic_logo, "5821"),
+
     /**
      * 真红大炮、Faiz
      * 机动天马 起飞
+     * Auto Vajin Take Off
      */
-    Temp5846(R.drawable.ic_logo, "5846"),
+    FaizAutoVajinTakeOff(R.drawable.ic_logo, "5846"),
+
     /**
      * 真红大炮、Faiz
      * 机动天马 战斗模式
-     * battle mode
+     * Auto Vajin Battle Mode
      */
-    Temp5826(R.drawable.ic_logo, "5826"),
+    FaizAutoVajinBattleMode(R.drawable.ic_logo, "5826"),
+
     /**
      * 真红大炮、Faiz
      * 机动天马 载具形态
+     * Auto Vajin Vehicle Mode
      */
-    Temp5886(R.drawable.ic_logo, "5886"),
+    FaizAutoVajinVehicleMode(R.drawable.ic_logo, "5886"),
 
     /**
-     * 真红大炮、Kaixa
+     * Kaixa
      * 疑似载具攻击
      */
     Temp9814(R.drawable.ic_logo, "9814"),
+
     /**
-     * 真红大炮、Kaixa
+     * Kaixa
      * 疑似召唤载具
      */
     Temp9821(R.drawable.ic_logo, "9821"),
+
     /**
-     * 真红大炮、Kaixa
+     * Kaixa
      * battle mode
      */
     Temp9826(R.drawable.ic_logo, "9826"),
+
     /**
-     * 真红大炮、Kaixa
+     * Kaixa
      * vehicle mode(战车模式)
      */
     Temp9886(R.drawable.ic_logo, "9886"),
@@ -159,20 +217,5 @@ enum class FaizOption(val icon: Int, val code: String = "", val command: Int = 0
      * 循环音效模式
      */
     Temp391(R.drawable.ic_logo, "391"),
-
-    /**
-     * 喷射滑行者 准备行动
-     */
-    Temp3814(R.drawable.ic_logo, "3814"),
-
-    /**
-     * 喷射滑行者 靠近
-     */
-    Temp3821(R.drawable.ic_logo, "3821"),
-
-    /**
-     * 喷射滑行者 起飞
-     */
-    Temp3846(R.drawable.ic_logo, "3846"),
 
 }
